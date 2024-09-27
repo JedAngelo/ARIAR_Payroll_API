b39a45f67d6638f02b1218d19f553e78af41fe36
JedAngelo <tanjedangelo@gmail.com>
2024-09-27 15:25:22 +0800

    bruh

--- Payroll_Library/Models/Dto/EmployeeDto/ContactInformationDto.cs
+++ Payroll_Library/Models/Dto/EmployeeDto/ContactInformationDto.cs
@@ -8,15 +8,15 @@ namespace Payroll_Library.Models.Dto.EmployeeDto
 {
     public class ContactInformationDto
     {
-        public int ContactId { get; set; }
+            public int ContactId { get; set; }
 
-        public Guid PersonalId { get; set; }
+            public Guid PersonalId { get; set; }
 
-        public string Address { get; set; } = null!;
+            public string Address { get; set; } = null!;
 
-        public string PhoneNumber { get; set; } = null!;
+            public string PhoneNumber { get; set; } = null!;
 
-        public string Email { get; set; } = null!;
+            public string Email { get; set; } = null!;
 
         //public virtual PersonalInformationDto PersonalDtos { get; set; } = null!;
     }
